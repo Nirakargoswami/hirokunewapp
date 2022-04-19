a676477a6e685cddaec2fc002de8b9fd33670501
nirakar <goswaminirakar65.com>
2022-04-19 12:58:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const importData = require("./data.json")
+let port = process.env.PORT || 3000 ;
+
+app.get("/",(req,res) => {
+    res.send("hellow world")
+});
+
+app.get("/player",(req,res) => {
+    res.send(importData);
+});
+
+app.listen(port,() =>{
+    console.log(`exapmle listing on ${port}`)
+})
